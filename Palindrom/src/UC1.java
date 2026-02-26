2f16ea25f07a51366b6637827c5cc23ad30f7f48
sr6749-source <sr6749@srmist.edu.in>
2026-02-26 10:30:54 +0530

    Shivanka Rathore completed UC8

--- Palindrom/src/UC1.java
+++ Palindrom/src/UC1.java
@@ -1,22 +1,21 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
 public class UC1 {
     static void main() {
-                String input = "refer";
-                System.out.println("Input : " + input);
-                Deque<Character> deque = new ArrayDeque<>();
-                for (char c : input.toCharArray()) {
-                    deque.addLast(c);
-                }
-                boolean isPalindrome = true;
-                while (deque.size() > 1) {
-                    char first = deque.removeFirst();
-                    char last = deque.removeLast();
-                    if (first != last) {
-                        isPalindrome = false;
-                        break;
-                    }
-                }
-                System.out.println("Is Palindrome? : " + isPalindrome);
+        String input = "level";
+        System.out.println("Input : " + input);
+        LinkedList<Character> list = new LinkedList<>();
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+        boolean isPalindrome = true;
+        while (list.size() > 1) {
+            Character first = list.removeFirst();
+            Character last = list.removeLast();
+            if (!first.equals(last)) {
+                isPalindrome = false;
+                break;
             }
         }
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
